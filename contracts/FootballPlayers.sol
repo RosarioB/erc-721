e15a514c7fbd0133d16eb97ffd2f281fe71bddc3
Rosario Borgesi <borgesiros@gmail.com>
2024-03-30 16:20:34 +0100

    Added URI storage, enumerable, burnable and pausable extensions

--- contracts/FootballPlayers.sol
+++ contracts/FootballPlayers.sol
@@ -3,10 +3,13 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract FootballPlayers is ERC721, Ownable {
+contract FootballPlayers is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausable, Ownable, ERC721Burnable {
     uint16 private TOKEN_CAP = 4;
     uint256 private _nextTokenId;
 
@@ -15,18 +18,53 @@ contract FootballPlayers is ERC721, Ownable {
         Ownable(msg.sender)
     {}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://bafybeiag6fokmiz6xmjodjyeuejtgrbyf2moirydovrew2bhmxjrehernq/";
+    function pause() public onlyOwner {
+        _pause();
     }
 
-    function safeMint(address to) public onlyOwner {
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
         require (_nextTokenId <= TOKEN_CAP, "Token cap exceeded");
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        _requireOwned(tokenId);
-        return string(abi.encodePacked(_baseURI(), Strings.toString(tokenId), ".json"));
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- test/FootballPlayers.ts
+++ test/FootballPlayers.ts
@@ -27,59 +27,74 @@ describe("FootballPlayers", () => {
 
   describe("Minting", () => {
     it("Should mint a new token and assign it to owner", async () => {
-      await footballPlayers.safeMint(owner.address);
+      await footballPlayers.safeMint(owner.address, "an IPFS address");
       expect(await footballPlayers.ownerOf(0)).to.equal(owner.address);
     });
 
     it("Should fail if non-owner tries to mint", async () => {
-      await expect(footballPlayers.connect(addr1).safeMint(addr1.address))
+      await expect(footballPlayers.connect(addr1).safeMint(addr1.address, "an IPFS address"))
       .to.be.revertedWithCustomError(footballPlayers, "OwnableUnauthorizedAccount");
     });
 
     it("Should auto increment tokenId with each new minted token", async () => {
-        await footballPlayers.safeMint(await owner.getAddress());
+        await footballPlayers.safeMint(await owner.getAddress(), "an IPFS address");
         expect(await footballPlayers.ownerOf(0)).to.equal(await owner.getAddress());
   
-        await footballPlayers.safeMint(await owner.getAddress());
+        await footballPlayers.safeMint(await owner.getAddress(), "an IPFS address");
         expect(await footballPlayers.ownerOf(1)).to.equal(await owner.getAddress());
     });
 
     it("Should not mint more than TOKEN_CAP tokens", async () => {
       const TOKEN_CAP = 4;
       for (let i = 0; i <= TOKEN_CAP; i++) {
-        await footballPlayers.safeMint(owner.address);
+        await footballPlayers.safeMint(owner.address, "an IPFS address");
       }
-      await expect(footballPlayers.safeMint(owner.address)).to.be.revertedWith("Token cap exceeded");
+      await expect(footballPlayers.safeMint(owner.address, "an IPFS address")).to.be.revertedWith("Token cap exceeded");
     });
 
     it("Should return the right tokenURI", async () => {
-      const BASE_URI: String = "ipfs://bafybeiag6fokmiz6xmjodjyeuejtgrbyf2moirydovrew2bhmxjrehernq/"
-      await footballPlayers.safeMint(owner.address);
-      expect(await footballPlayers.tokenURI(0)).to.equal(BASE_URI + "0.json");
+      await footballPlayers.safeMint(owner.address, "an IPFS address");
+      expect(await footballPlayers.tokenURI(0)).to.equal("an IPFS address");
     });
   });
 
   describe("Token transferring", () => {
     it("Should transfer a token from one address to another", async () => {
-        await footballPlayers.safeMint(await owner.getAddress());
+        await footballPlayers.safeMint(await owner.getAddress(), "an IPFS address");
         await footballPlayers["safeTransferFrom(address,address,uint256)"](owner, addr1, 0);
         expect(await footballPlayers.ownerOf(0)).to.equal(await addr1.getAddress());
     });
   
     it("Should revert when trying to transfer a token that is not owned", async () => {
-      await footballPlayers.safeMint(await owner.getAddress());
+      await footballPlayers.safeMint(await owner.getAddress(), "an IPFS address");
       await expect(
         footballPlayers.connect(addr1)["safeTransferFrom(address,address,uint256)"] (addr1, addr2, 0)
       ).to.be.revertedWithCustomError(footballPlayers, "ERC721InsufficientApproval");
     });
 
     it("Should not send tokens to the 0 address", async () => {
-      await footballPlayers.safeMint(await owner.getAddress());
+      await footballPlayers.safeMint(await owner.getAddress(), "an IPFS address");
       await expect(
         footballPlayers["safeTransferFrom(address,address,uint256)"] (owner, ethers.ZeroAddress, 0)
       ).to.be.revertedWithCustomError(footballPlayers, "ERC721InvalidReceiver");
     });
   });
-    
+
+  describe("Enumeration", () => {
+    it("Should return the right total supply of tokens", async () => {
+      await footballPlayers.safeMint(owner.address, "an IPFS address");
+      await footballPlayers.safeMint(owner.address, "an IPFS address");
+      expect(await footballPlayers.totalSupply()).to.equal(2);
+    });
+  });
+
+  describe("Burning", () => {
+    it("Should correctly burn a token", async () => {
+      await footballPlayers.safeMint(owner.address, "an IPFS address");
+      await footballPlayers.burn(0);
+      expect(await footballPlayers.totalSupply()).to.equal(0);
+      await expect(footballPlayers.ownerOf(0)).to.be.revertedWithCustomError(footballPlayers, "ERC721NonexistentToken");
+    });
+  }); 
   // Add more tests for other functionalities of your contract.
 });
